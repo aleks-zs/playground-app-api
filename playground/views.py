334cba2a640999fbdd22d02d7261fdb1c590ad7e
Aleks <aleks@JustComputer.local>
2023-01-29 12:31:19 +0200

    simple api + product model

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render # noqa
+from django.http import JsonResponse
 
-# Create your views here.
+
+def api_simple(request, *args, **kwargs):
+    body = request.body
+    headers = request.headers
+    print(request.GET)
+    print(headers['content_type'])
+    print(body)
+    return JsonResponse({'message': 'API request recived'})
