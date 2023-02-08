14aa71fdf52d418e28b782e1b219b423dba5f19f
Aleks <aleks@JustComputer.local>
2023-02-08 13:23:40 +0200

    get all products test

--- playground/tests/test_playground_api.py
+++ playground/tests/test_playground_api.py
@@ -1,4 +1,3 @@
-'''
 from django.test import TestCase
 from django.urls import reverse
 
@@ -7,4 +6,57 @@ from rest_framework.test import APIClient
 
 from core.models import Product
 from playground.serializers import ProductSerializer
-'''
+
+from decimal import Decimal
+
+
+ALL_PRODUCTS_URL = reverse('playground:all-products')
+
+
+def detail_url(product_id):
+    return reverse('product-detail', args=[product_id])
+
+
+def create_product(**params):
+    return Product.objects.create(**params)
+
+
+class ProductApiTests(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
+        create_product(
+            title='FirstTitle',
+            content='FirstContent',
+            price=Decimal('1.00')
+        )
+        create_product(
+            title='SecondTitle',
+            content='SecondContent',
+            price=Decimal('2.00')
+        )
+
+    def test_get_all_products(self):
+        res = self.client.get(ALL_PRODUCTS_URL)
+
+        products = Product.objects.all().order_by('id')
+        serializer = ProductSerializer(products, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_get_product_by_id(self):
+        pass
+
+    def test_create_product(self):
+        pass
+
+    def test_update_whole_product(self):
+        pass
+
+    def test_partially_update_the_product(self):
+        pass
+
+    def test_delete_the_product(self):
+        pass
--- playground/urls.py
+++ playground/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from .views import ProductList, ProductDetails
 
 
+app_name = 'playground'
+
 urlpatterns = [
-    path('product-list', ProductList.as_view()),
-    path('product-details/<int:pk>', ProductDetails.as_view())
+    path('product-list', ProductList.as_view(), name='all-products'),
+    path('product-details/<int:pk>', ProductDetails.as_view(), name='product-detail') # noqa
 ]
